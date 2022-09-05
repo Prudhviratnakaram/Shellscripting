eea260ae04bdcc14439a3bd868279bab948eb556
Prudhviratnakaram <ratnakaramprudhvi@gmail.com>
2022-09-05 12:46:20 -0500

    user

--- input.sh
+++ input.sh
@@ -1,3 +1,7 @@
-echo "Enter the name " : name
+echo "Enter the name "
 read name
 echo "the name is " $name
+evho "taking the input in same line"
+echo "enter the name"
+read -p name
+echo "the name is "$name
