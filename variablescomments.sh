8672e0c639d9b8d55b0dc1857dedc3e366351e15
Prudhviratnakaram <ratnakaramprudhvi@gmail.com>
2022-09-05 12:25:59 -0500

    user

--- variablescomments.sh
+++ variablescomments.sh
@@ -1,6 +1,8 @@
 echo "Helloworld"
 echo "System Variables"
-echo $BASH
-echo $BASH_VERSION
-echo $HOME
-echo $PWD
+echo our shell name is $BASH
+echo our bash nam e is $BASH_VERSION
+echo our home directory is $HOME
+echo our present directory is $PWD
+name=Prudhvi
+echo "name is" $name
