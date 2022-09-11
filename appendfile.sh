c4edc9a4cd0cb64875dd0206b9635baee4936c58
Prudhviratnakaram <ratnakaramprudhvi@gmail.com>
2022-09-11 17:59:22 -0500

    user

---
+++ appendfile.sh
@@ -0,0 +1,11 @@
+echo "enter the file filename"
+read $filename
+ if [ -f "$filename" ];then
+   if [ -w "$filename" ];then
+     echo "type some text in to  the file"
+     cat >>$filename
+    else
+      echo "file does not have the write permisssions"
+    fi
+    echo "file not found"
+ fi
