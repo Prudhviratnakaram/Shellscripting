9987cafcd04be8d5171a8e991eadbd8f434450c8
Prudhviratnakaram <ratnakaramprudhvi@gmail.com>
2022-09-12 17:30:24 -0500

    user

---
+++ casestatementex.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+echo "enter some character "
+read value
+
+
+case $value in
+  [A-Z] )
+  echo "this is a $Value in A to Z" ;;
+  [a-z] )
+  echo "this is a $Value in a to z" ;;
+  [0-9])
+  echo "this is a $Value in 0 to 9" ;;
+  ?)
+  echo "this is a $Value of special character" ;;
+  * )
+    echo "unknown veichle" ;;
+esac
+
