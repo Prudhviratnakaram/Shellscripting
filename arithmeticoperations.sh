ff68aa3e85e6aa5767a90863215fefe31f1837c4
Prudhviratnakaram <ratnakaramprudhvi@gmail.com>
2022-09-12 16:52:01 -0500

    user

--- arithmeticoperations.sh
+++ arithmeticoperations.sh
@@ -6,6 +6,13 @@ echo $(( num1 - num2 ))
 echo $(( num1 * num2 ))
 echo $(( num1 / num2 ))
 echo $(( num1 % num2 ))
+echo another way tp do it
+echo $(expr $num1 + $num2 )
+echo $(expr $num1 - $num2 )
+echo $(expr $num1 * $num2 )
+echo $(expr $num1 / $num2 )
+echo $(expr $num1 % $num2 )
+
 
 
 
