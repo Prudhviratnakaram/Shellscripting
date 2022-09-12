efb255d80ccd66e5e719031c9a8d50364027148f
Prudhviratnakaram <ratnakaramprudhvi@gmail.com>
2022-09-12 17:51:19 -0500

    user

--- array.sh
+++ array.sh
@@ -1,9 +1,9 @@
 os=('ubuntu','windows','linux')
 echo replacing the 3rd index value
 os[3]='mac'
-unset os[2]
+
 echo The elements in the array is "${os[@]}"
-echo  The zeroth index in the array is"${os[0]}"
+echo  The zeroth index in the array is "${os[0]}"
 echo The indexes of the arrays are "${!os[@]}"
 echo The number of elements in the array are "${#os[@]}"
 string=aasddfdf
