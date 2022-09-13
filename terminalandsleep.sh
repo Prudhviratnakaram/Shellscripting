f9bc822d893079d239afd4222978d6b6c738896d
Prudhviratnakaram <ratnakaramprudhvi@gmail.com>
2022-09-13 18:09:46 -0500

    user

--- terminalandsleep.sh
+++ terminalandsleep.sh
@@ -1,8 +1,10 @@
 n=1;
-while [ $n -le 10 ]
+while [ $n -le 3 ]
 do
   echo $n
   n=$((n+1))
   # pause and print the output after one second
   sleep 1
+  #opens the gnome terminal for 3 times
+  gnome-terminal &
 done
