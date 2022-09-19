2773f018a698f3786aa3f672ae91ac2f633a33c2
Prudhviratnakaram <ratnakaramprudhvi@gmail.com>
2022-09-19 14:43:26 -0500

    user

--- select.sh
+++ select.sh
@@ -2,3 +2,20 @@ select name in mark john tom ben
 do
   echo "$name selected"
 done
+select name in mark john tom ben
+do
+  case $name in
+  mark)
+    echo mark selected
+    ;;
+  john)
+    echo john selected
+    ;;
+  ben)
+    echo ben selected
+    ;;
+  *)
+    echo "Error in providing the number between 1..4"
+    esac
+
+done
