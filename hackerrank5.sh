058451aede85f62b920b50150b9fc1ca07492d78
Prudhviratnakaram <ratnakaramprudhvi@gmail.com>
2022-09-20 13:59:54 -0500

    user

--- hackerrank5.sh
+++ hackerrank5.sh
@@ -1,11 +1,8 @@
 read value
 
-if(( "${value}" == "y" || "${value}" == "Y"  || "${value}" == "n" || "${value}" == "N" ))
-then
    if(( "${value}" == "y" || "${value}" == "Y" ))
    then
     echo YES
    else
    echo NO
   fi
-fi
