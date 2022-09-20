5cf269b6c325885cf9d2bd4c2cc6d707d437094c
Prudhviratnakaram <ratnakaramprudhvi@gmail.com>
2022-09-20 17:59:35 -0500

    user

--- hackerrang6.sh
+++ hackerrang6.sh
@@ -3,7 +3,7 @@ read Y
 read Z
 if(("$X" <= "100" && "$X" >= "1" && "$Y" <= "100" && "$Y" >= "1" && "$Z" <= "100" && "$Z" >= "1" &&  $((X+Y))>$Z && $((X+Z))>$Y && $((Z+Y))>$X ))
 then
-   if (( "$X" == "$Y"))
+   if [ "$X" -eq "$Y" ]
      then
        echo EQUILATERAL
     fi
