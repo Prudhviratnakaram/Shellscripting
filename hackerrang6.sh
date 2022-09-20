318a3afe1f73a80b5c69eb327f9fd1400099b854
Prudhviratnakaram <ratnakaramprudhvi@gmail.com>
2022-09-20 18:26:13 -0500

    user

--- hackerrang6.sh
+++ hackerrang6.sh
@@ -3,16 +3,15 @@ read Y
 read Z
 if(("$X" <= "100" && "$X" >= "1" && "$Y" <= "100" && "$Y" >= "1" && "$Z" <= "100" && "$Z" >= "1" &&  $((X+Y))>$Z && $((X+Z))>$Y && $((Z+Y))>$X ))
 then
-   if [ "$X" -eq "$Y" -a "$Y" -eq "$Z" -a "$X" -eq "$Z" ]
+   if [ "$X" -eq "$Y" -a "$Y" -eq "$Z" ]
      then
        echo EQUILATERAL
 
-    elif [ "$X" -eq "$Y" -o "$z" -o "$Y" -eq "$Z" -o "$Z" -eq "$X" ] -ne [ "$X" -eq "$Y" -a "$Y" -eq "$Z" -a "$X" -eq "$Z" ]
+    elif [ "$X" -eq "$Y" -o "$y" -eq "$z"  -o "$Z" -eq "$X" ]
         then
           echo ISOSCELES
     fi
     else
            echo SCALENE
-
 
 fi
