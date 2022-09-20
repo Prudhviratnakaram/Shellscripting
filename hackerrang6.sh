f90a32e36b54ad0e8ccd76d391b68e468bdd5547
Prudhviratnakaram <ratnakaramprudhvi@gmail.com>
2022-09-20 17:32:31 -0500

    user

---
+++ hackerrang6.sh
@@ -0,0 +1,10 @@
+read X
+read Y
+read Z
+if (("$X" <= "100" && "$X" >= "1" && "$Y" <= "100" && "$Y" >= "1" && "$Z" <= "100" && "$Z" >= "1" ))
+then
+   if (($X==$Y==$Z))
+     then
+       echo EQUILATERAL
+       fi
+fi
